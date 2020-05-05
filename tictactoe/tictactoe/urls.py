05ce440d8b95e2e19ff99cb34088294b6cbaa033
Julia Thea <yuliya.boyadjan@gmail.com>
2020-05-05 14:46:16 -0500

    added url to home page

--- tictactoe/tictactoe/urls.py
+++ tictactoe/tictactoe/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from . import views
+from .views import welcome
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', welcome)
 ]
