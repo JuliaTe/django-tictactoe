ae588f20e390302df1faf215ac3ecb7a64cdb3ab
Julia Thea <yuliya.boyadjan@gmail.com>
2020-05-07 15:00:33 -0500

    stuff

--- tictactoe/gameplay/admin.py
+++ tictactoe/gameplay/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from .models import Game, Move
 
-admin.site.register(Game)
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+  list_display = ('id', 'first_player', 'second_player', 'status')
+  list_editable = ('status', 'first_player', 'second_player')
+
 admin.site.register(Move)
