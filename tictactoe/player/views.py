77396704ec783e1a9d92a03206f80b8c8e3fe6c0
Julia Thea <yuliya.boyadjan@gmail.com>
2020-05-08 11:42:42 -0500

    updated views

--- tictactoe/player/views.py
+++ tictactoe/player/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from gameplay.models import Game
 
 # Create your views here.
 
 def home(request):
-  return render(request, "player/home.html")
+  my_game = Game.objects.games_for_user(request.user)
+  active_games = my_game.active()
+
+  return render(request, "player/home.html",
+               { 'games': active_games})
 
