4fb2441234aef3681b38349ae668f874827eb0a1
Julia Thea <yuliya.boyadjan@gmail.com>
2020-05-08 11:43:23 -0500

    updated models

--- tictactoe/gameplay/models.py
+++ tictactoe/gameplay/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from django.db import models 
+from django.db.models import Q
 from django.contrib.auth.models import User
 
 GAME_STATUS_CHOICES = (
@@ -9,6 +10,18 @@ GAME_STATUS_CHOICES = (
   ('D', 'Draw')
 )
 
+class GameQuerySet(models.QuerySet):
+  def games_for_user(self, user):
+
+    return self.filter(
+      Q(first_player=user) | Q(second_player=user)
+    )
+
+  def active(self):
+    return self.filter(
+      Q(status='F') | Q(status='S')
+    )
+
 class Game(models.Model):
   first_player = models.ForeignKey(User,
                                     related_name="games_first_player", on_delete=models.CASCADE)
@@ -18,6 +31,8 @@ class Game(models.Model):
   last_active = models.DateTimeField(auto_now=True)
   status = models.CharField(max_length=1, default='F',
                               choices=GAME_STATUS_CHOICES) #added choices
+  
+  objects = GameQuerySet.as_manager()
 
   def __str__(self):
     return "{0} vs {1}".format(
